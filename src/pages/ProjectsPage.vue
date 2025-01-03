cccff1e4b81aa9d7f852e3a750f7868f55c2fff0
Artem <artem1991v120fps@gmail.com>
2025-01-04 00:19:27 +0300

    вывел свои проекты

--- src/pages/ProjectsPage.vue
+++ src/pages/ProjectsPage.vue
@@ -2,12 +2,17 @@
   <div class="projects-page-wrapper">
     <div class="projects-page">
       <h1 class="projects__title">Мои проекты:</h1>
+      <ul class="projects__list">
+        <li v-for="item in ProjectsPage.projects" :key="item.title">
+          <a :href="item.link" target="_blank">{{ item.title }}</a>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script setup>
-
+import ProjectsPage from '../../server/ProjectsPage';
 </script>
 
 <style lang="scss" scoped>
@@ -23,7 +28,13 @@
     max-width: 75%;
 
     .projects__title {
-      margin: 100px 0;
+      margin: 100px 0 50px;
+    }
+
+    .projects__list {
+      display: flex;
+      flex-direction: column;
+      gap: 10px;
     }
   }
 }
